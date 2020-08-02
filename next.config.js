5169e4d1063f0d14176cec1bfd69d094e58a56ee
Sebastian <sebastian.luque.pl@gmail.com>
2020-08-01 22:05:26 -0500

    draft

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 
-module.exports = {
-  env: {
-    API_URL: 'my-value',
-  },
-}
-
 module.exports = withPlugins([[optimizedImages, {}]], {
   env: {
     API_URL: 'my-value',
